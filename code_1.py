c1f89eb219ecd81fdddff6c65c488924d0c84eb3
Roman Lazarev <144658461+romalazar@users.noreply.github.com>
2023-12-02 23:59:38 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,54 @@
+        }
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+
+        int[] data = new int[10];
+public class RandomDataGenerator {
+    }
+
+        for (int item : data) {        console.log("Random Number: " + item);
+
+    const data = generateRandomData();
+
+    for (let i = 0; i < 10; i++) {
+    data.forEach(item => {
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+}
+    });
+    }
+function generateRandomData() {
+    const data = [];
+
+main();
+}    public static void main(String[] args) {
+        }
+        for (int item : data) {
+        }
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+
+    }
+        int[] data = new int[10];
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+
+
+def main():
+        print(f"Random Number: {item}")
+
+    return data
+import random
+    main()
+if __name__ == "__main__":
+    data = generate_random_data()
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
